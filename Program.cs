373d548548c1e6a8eefd29c9f6ec99e1b39ad349
Erwin de Vreugd <erwindevreugd@users.noreply.github.com>
2016-05-01 08:12:05 +0200

    Initial project upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace argsecho
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var count = 0;
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"{i} : '{args[i]}'");
+                count++;
+            }
+
+            Console.WriteLine($"{count} argument(s)");
+            Console.WriteLine("Press any key to exit");
+
+            Console.ReadKey(true);
+        }
+    }
+}
